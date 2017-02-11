be256e8add673fb4c6149a1736541d69d4fc371a
Greg Lee <gml2136@gmail.com>
2017-02-11 12:30:27 -0800

    deleted some whitespace

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// create a question constructor
+
+function Question(body, correct, diversion1, diversion2, diversion3){
+  this.body = body;
+  this.correct = correct;
+  this.diversion1 = diversion1;
+  this.diversion2 = diversion2;
+  this.diversion3 = diversion3;
+
+
+  //methods
+
+}
