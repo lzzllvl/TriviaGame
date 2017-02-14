6261e4eac52b99d6adb53e1cd9c430aeccb1efe6
Greg Lee <gml2136@gmail.com>
2017-02-13 19:34:46 -0800

    Just getting started, added bones to html and a first iteration of the game and question objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,35 @@
 // create a question constructor
 
-function Question(body, correct, diversion1, diversion2, diversion3){
+function Question(body, a, b, c, d, correctKey){
   this.body = body;
-  this.correct = correct;
-  this.diversion1 = diversion1;
-  this.diversion2 = diversion2;
-  this.diversion3 = diversion3;
+  this.a = a;
+  this.b = b;
+  this.c = c;
+  this.d = d;
+  this.correctKey = correctKey;
 
-
-  //methods
+  //Object methods
 
 }
+
+let questionObjectArray = [];
+
+//making a test Question
+let test = new Question("This is a Test Question", "first", "second", "third", "fourth", "B");
+questionObjectArray.push(test);
+//create the game object literal, only need one so no constructor
+//this is performing controller logic
+const game = {
+  //properties
+  state: null,
+  time: 0,
+
+  //methods
+  reset: ()=> {},
+  submitAnswer: ()=> {},
+  checkAnswer: ()=> {},
+  getNextQuestion: ()=> {},
+  startTimer: ()=> {},
+  stopTimer: ()=> {},
+
+};
