cbc45ae4f45fdca5d24dafac78996de53afdaf91
Greg Lee <gml2136@gmail.com>
2017-02-14 23:50:54 -0800

    Got some of the form html set up, still need to handle the on submit event

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,4 @@
 // create a question constructor
-
 function Question(body, a, b, c, d, correctKey){
   this.body = body;
   this.a = a;
@@ -25,15 +24,25 @@ let questionObjectArray = [];
 //making a test Question
 const test = new Question("This is a Test Question", "first", "second", "third", "fourth", "b");
 questionObjectArray.push(test);
-const q1  = new Question("How many planets are in the Solar System?", "Eight", "Seven", "Nine", "Infinite", "a");
+const q1  = new Question("How many planets are in the Solar System?", "Eight", "Seven", "Nine", "Too Many To Count", "a");
 questionObjectArray.push(q1);
 const q2 = new Question("What is the Nearest Galaxy to our Own?", "Olsec-1", "Milky Way", "Andromeda", "The Degoba System", "c");
 questionObjectArray.push(q2);
-
-//need to make an intervalID for setting timer
-
+// const q3 = new Question("What is the largest planet in the solar system?", "Uranus", "Mars", "Saturn", "Jupiter", "d");
+// questionObjectArray.push(q3);
+// const q4 = new Question("Which type of celestial body lies at the center of our galaxy?", "Star", "Black Hole", "Gas Cloud", "Pulsar", "b");
+// questionObjectArray.push(q4);
+// const q5 = new Question("Where do our solar system's comets come from?", "They break off of large moons", "The Asteroid Belt", "The Ort Cloud", "Cthulhu", "c");
+// questionObjectArray.push(q5);
+// const q6 = new Question("How long does it take light from the Sun to reach Earth", "15 seconds", "8 minutes", "1-2 hours", "25 milliseconds", "b");
+// questionObjectArray.push(q6);
+// const q7 = new Question("What protects life on planet Earth is protected from solar winds", "Earth's Magnetic Field", "The Ionosphere", "The Moon", "A Solar WindBreaker", "a");
+// questionObjectArray.push(q7);
+// const q8 = new Question("Which man-made spacecraft to has been able to enter interstellar space?", "Pathfinder", "Juno", "Voyager 1", "No Satellite has gone that far", "c");
+// questionObjectArray.push(q8);
 
 //create the game object literal, only need one so no constructor
+
 //this is performing controller logic
 const game = {
   //properties
@@ -50,7 +59,7 @@ const game = {
   init: () => {
     game.current = game.getQuestion();
     game.state = false;
-    game.timeLimit = 10000; //ms
+    game.timeLimit = 8000; //ms
     game.timeLeft = game.timeLimit / 1000; // this is to display the seconds
     game.correctAnswers = 0;
     game.incorrectAnswers = 0;
@@ -77,8 +86,8 @@ const game = {
       game.incorrectAnswers++;
     }
     game.current = game.getQuestion();
-    game.displayQuestion();
-    game.addAnswerClicks();
+    // game.displayQuestion();
+    // game.addAnswerClicks();
     game.resetTimer();
   },
 
@@ -89,9 +98,11 @@ const game = {
 
   getQuestion: () => {
     //returns a random question from the question array.
-    let q = questionObjectArray.splice(Math.floor(Math.random() * questionObjectArray.length), 1 )
-    q = q[0];//splice is going to return a single item array;
-    return q;
+    if(!game.isGameOver()){
+      let q = questionObjectArray.splice(Math.floor(Math.random() * questionObjectArray.length), 1 )
+      q = q[0];//splice is going to return a single item array;
+      return q;
+    }
   },
 
   startTimer: () => {
@@ -104,7 +115,11 @@ const game = {
 
   countDown: () => {
     game.timeLeft--;
-    $("#timer").html(game.timeLeft);
+    if(game.timeLeft>=0){
+      $("#timer").html(game.timeLeft);
+    } else {
+      game.stopTimer();
+    }
   },
 
   addAnswerClicks: () => {
@@ -128,7 +143,12 @@ const game = {
 
   isGameOver: () => {
     if(questionObjectArray.length == 0){
+      game.state = false;
+      return true;
       console.log("question Array empty");
+    } else {
+      game.state = true;
+      return false;
     }
   }
 };
@@ -141,5 +161,10 @@ const game = {
 // let current = game.getQuestion();
 // current = current[0]; // get question returns a one item array
 
-
-game.init();
+$("form").hide()
+$("button").on("click", () => {
+  game.init();
+  game.startGame();
+  $("form").show();
+  $("button").hide();
+});
